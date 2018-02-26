2e76b9fdb88fef0092576892f3dbe84fd08207a6
dmitriy <topovikgroup@gmail.com>
2018-02-26 02:10:48 +0200

    result commit

---
+++ index1.js
@@ -0,0 +1,5 @@
+import { Hellower } from 'hwork_add';
+const hellower = new Hellower();
+hellower.print('One');
+hellower.print('Two');
+hellower.print('Three');
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    entry: './index1.js',
+    output: {
+      filename: 'bundle.js'
+    }
+  }
